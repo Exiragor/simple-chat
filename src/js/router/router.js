9cd9dcc9dbc3a4d89096e8c5fd5f2d44670c1f47
Exiragor <winden999@gmail.com>
2018-05-10 16:26:23 +0300

    Update router.js

--- src/js/router/router.js
+++ src/js/router/router.js
@@ -2,13 +2,11 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 import Home from '@/pages/Home.vue'
-import List from '@/pages/List.vue'
 
 Vue.use(Router);
 
 const routes = [
   { path: '*', component: Home },
-  { path: '/list', component: List },
 ];
 
 export default new Router({
